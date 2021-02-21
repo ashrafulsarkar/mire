36cc7cdeefdb5fcfd7d33e36a9de5a6df5cebfd5
ashrafulsarkar <ashrafulsarkar47@gmail.com>
2021-02-22 01:32:48 +0600

    login popup add

--- js/main.js
+++ js/main.js
@@ -8,6 +8,21 @@
         $( ".input_part input" ).blur(function() {
             $( ".search_sugg" ).addClass('d-none');
         });
+
+        //password show hide
+        $("#show_hide_password a").on('click', function(event) {
+            event.preventDefault();
+            if($('#show_hide_password input').attr("type") == "text"){
+                $('#show_hide_password input').attr('type', 'password');
+                $('#show_hide_password i').addClass( "fa-eye-slash" );
+                $('#show_hide_password i').removeClass( "fa-eye" );
+            }else if($('#show_hide_password input').attr("type") == "password"){
+                $('#show_hide_password input').attr('type', 'text');
+                $('#show_hide_password i').removeClass( "fa-eye-slash" );
+                $('#show_hide_password i').addClass( "fa-eye" );
+            }
+        });
+        
         // owl carousel review part
         $('.review-carousel').owlCarousel({
             loop:true,
